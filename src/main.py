438be97f68ce33a50ff0e709384f16afccc1f52c
ovinvcode <ovinvidanapathirana@gmail.com>
2025-09-19 21:37:42 +0530

    Initial project setup

---
+++ src/main.py
@@ -0,0 +1,8 @@
+# src/main.py
+import colorama
+from colorama import Fore
+
+# Initialize colorama
+colorama.init(autoreset=True)
+
+print(Fore.GREEN + "Setup Complete! The virtual environment and libraries are working.")
